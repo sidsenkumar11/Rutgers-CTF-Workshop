e28bc14db880ae69ef9a630de1ee5b9c0c9619d5
Sid Senthilkumar <pikahero2@gmail.com>
2024-10-07 06:54:10 -0400

    fix local flaskapp debugging

--- oauth_secrets/const.py
+++ oauth_secrets/const.py
@@ -1,4 +1,6 @@
 import os
+from dotenv import load_dotenv
+load_dotenv('.env')
 
 USERINFO_URL = "https://openidconnect.googleapis.com/v1/userinfo"
 OAUTH_PROVIDER_URL = "https://accounts.google.com/o/oauth2"
